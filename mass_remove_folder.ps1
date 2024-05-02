3f0705f33dbb9376defe4102e919bb93a41b9a25
Alon A. Rabinowitz <alonrab@outlook.com>
2024-05-02 16:52:48 +0300

    fin

--- mass_remove_folder.ps1
+++ mass_remove_folder.ps1
@@ -1,7 +1,7 @@
-$folderName = "node_modules"  # Replace with the name of the folder you're searching for
-$rootPath = "C:\Users\alonr\OneDrive\Shenkar\Archive\שנה ג סמסטר א\Methods_in_software_engenering"  # Replace with the path where you want to start the search
+$folderName = "folder"  # Replace with the name of the folder you're searching for
+$rootPath = "C:\"  # Replace with the path where you want to start the search
 
-# Find the folders
+# Find the folders and remove them
 Get-ChildItem -Path $rootPath -Recurse -Directory | Where-Object {$_.Name -eq $folderName} | ForEach-Object {
     Remove-Item -Path $_.FullName -Recurse -Force -Verbose
 }
