afb0b5d1ae93bc30067bf7db533acf87b2479838
Alon A. Rabinowitz <45129051+AlonRR@users.noreply.github.com>
2025-01-05 11:35:12 +0200

    enhance mass_remove_folder.ps1 with parameter validation and optional flags for WhatIf and Confirm

--- mass_remove_folder.ps1
+++ mass_remove_folder.ps1
@@ -1,7 +1,24 @@
-$folderName = "folder"  # Replace with the name of the folder you're searching for
-$rootPath = "C:\"  # Replace with the path where you want to start the search
+param (
+    [string]$folderName,
+    [string]$rootPath,
+    [switch]$WhatIf,
+    [switch]$Confirm
+)
+try {
+    # Check if folderName or rootPath were not specified
+    if (-not $folderName) {
+        throw "Error: folderName parameter is required."
+    }
 
-# Find the folders and remove them
-Get-ChildItem -Path $rootPath -Recurse -Directory | Where-Object {$_.Name -eq $folderName} | ForEach-Object {
-    Remove-Item -Path $_.FullName -Recurse -Force -Verbose
+    if (-not $rootPath) {
+        throw "Error: rootPath parameter is required."
+    }
+
+    # Find the folders and remove them
+    Get-ChildItem -Path $rootPath -Recurse -Directory | Where-Object { $_.Name -eq $folderName } | ForEach-Object {
+        Remove-Item -Path $_.FullName -Recurse -Force -Verbose -ErrorAction Stop -WhatIf:$WhatIf -Confirm:$Confirm
+    }
+}
+catch {
+    Write-Host $_.Exception.Message
 }
