cfa6229f1a606c62217e918451b84e398b1f8bf1
Alon A. Rabinowitz <alonrab@outlook.com>
2025-01-05 11:36:19 +0200

    add script to list folder sizes in descending order

---
+++ list_folders_size.ps1
@@ -0,0 +1,5 @@
+$fso = new-object -com Scripting.FileSystemObject
+Get-ChildItem -Directory `
+| Select-Object @{l = 'Size'; e = { $fso.GetFolder($_.FullName).Size } }, FullName `
+| Sort-Object Size -Descending `
+| Format-Table @{l = 'Size [MB]'; e = { '{0:N2}    ' -f ($_.Size / 1MB) } }, FullName
